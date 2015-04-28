b5f5659f5b27bb755875f53fa4c8e4b67cda0862
Tomasz Figa <tfiga@google.com>
2015-04-28 09:55:06 +0900

    CHROMIUM: rockchip: Add support for Android build system
    This patch adds changes necessary to enable building libdrm_rockchip
    inside Android build system.
    
    BUG=b:26864637
    TEST=N/A
    
    Change-Id: Ib9562f4891dfd5868ba4cb0b7acc20467a2d4084
    Signed-off-by: Tomasz Figa <tfiga@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/324674
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- libkms/Android.mk
+++ libkms/Android.mk
@@ -2,12 +2,14 @@ DRM_GPU_DRIVERS := $(strip $(filter-out swrast, $(BOARD_GPU_DRIVERS)))
 
 intel_drivers := i915 i965 i915g ilo
 radeon_drivers := r300g r600g radeonsi
+rockchip_drivers := rockchip
 nouveau_drivers := nouveau
 vmwgfx_drivers := vmwgfx
 
 valid_drivers := \
 	$(intel_drivers) \
 	$(radeon_drivers) \
+	$(rockchip_drivers) \
 	$(nouveau_drivers) \
 	$(vmwgfx_drivers)
 
